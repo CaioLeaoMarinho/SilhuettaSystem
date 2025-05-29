5a3177e6a3a33ff70d9813cb411ee2daaef96312
CaioLeaoMarinho <caiolmo10@gmail.com>
2025-05-29 12:08:36 -0300

    Criando projeto

---
+++ SilhuettaSystem/src/main/java/com/mycompany/silhuettasystem/SilhuettaSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.silhuettasystem;
+
+/**
+ *
+ * @author caiol
+ */
+public class SilhuettaSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
